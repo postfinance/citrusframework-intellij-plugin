0d45bdea178c50c6ea873a6c436df4d7db637312
Timon Borter <timon.borter@gmx.ch>
2025-09-15 20:08:40 +0200

    chore: refactor some code for better readability

---
+++ src/main/java/ch/postfinance/citrusframework/plugin/action/RunnerArgs.java
@@ -0,0 +1,10 @@
+package ch.postfinance.citrusframework.plugin.action;
+
+final class RunnerArgs {
+
+  static final String D_TESTS_TO_RUN = "-Dtests.to.run=";
+
+  private RunnerArgs() {
+    // Access to properties only
+  }
+}
